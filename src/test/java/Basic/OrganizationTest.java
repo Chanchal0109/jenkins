a5d43525e016ca3badf00afe95fc46d587ccaf36
Chanchal0109 <153546810+Chanchal0109@users.noreply.github.com>
2024-03-18 17:57:26 +0530

    upd

--- src/test/java/Basic/OrganizationTest.java
+++ src/test/java/Basic/OrganizationTest.java
@@ -1,17 +1,46 @@
 package Basic;
 
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class OrganizationTest {
-	
-	@Test
-	public void CreateOrganizationTest()
-	{
-		//launch the browser
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://localhost:8888/");
-	}
+import commonutils.PropertyFileUtil;
+import commonutils.WebDriverUtil;
 
+public class OrganizationTest {
+
+	
+
+	
+		//PropertyFileUtil pu = new PropertyFileUtil();
+		
+		//WebDriverUtil wu = new WebDriverUtil();
+		
+		
+		@Test
+		public void organizationTest() throws IOException, InterruptedException {
+			
+			WebDriver d = new ChromeDriver();
+			d.manage().window().maximize();
+			d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			d.get("http://localhost:8888/");
+			d.findElement(By.name("user_name")).sendKeys("admin");
+			d.findElement(By.name("user_password")).sendKeys("admin");
+			d.findElement(By.id("submitButton")).click();
+			
+//			WebDriver d = new ChromeDriver();
+//			wu.maximize(d);
+//			d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//			//TO READ THE DATA FROM PROPERTY FILE
+//			String url = pu.getDataFromProertyfile("Url");
+//			String username = pu.getDataFromProertyfile("Username");
+//			String pass = pu.getDataFromProertyfile("Password");
+//			
+		
+	}
+}
 }
